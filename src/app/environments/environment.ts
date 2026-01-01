c2211499d7d61116698abad88e2e76a14a818831
pandeybhargava <pandey.bhargava@gmail.com>
2026-01-01 20:10:21 +0530

    Remove client secret

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   azureAd: {
-    clientId: '593e8a96-fd68-4421-a548-e0a5f525b78f',
-    tenantId: ' 44cc3e29-963c-4dce-b794-f2137647b0d8',
+    clientId: '<<your clinet ID>>',
+    tenantId: ' <<tenantId>>',
     redirectUri: 'http://localhost:4200',
-    authority: 'https://login.microsoftonline.com/ 44cc3e29-963c-4dce-b794-f2137647b0d8',
+    authority: 'https://login.microsoftonline.com/<<tenantId>>',
     scopes: ['user.read']
   }
 };
