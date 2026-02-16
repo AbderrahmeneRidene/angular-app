d9e1267b784487e897656f8406a0d5834043f720
AbdRidene <abd.ridene@gmail.com>
2026-02-16 12:55:32 +0100

    second appdate off server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'dist/azure-ad-bootstrap/browser/index.html')));
+app.use(express.static(path.join(__dirname, 'dist/azure-ad-bootstrap/browser')));
 
 app.get('/*', (req, res) => {
   res.sendFile(
