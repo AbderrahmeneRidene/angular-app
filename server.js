43a7756d238d4034f6f1e7e298038227e6d18901
AbdRidene <abd.ridene@gmail.com>
2026-02-16 13:46:11 +0100

    second commit

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Attention au chemin exact de ton build Angular
 const distFolder = path.join(__dirname, 'dist/azure-ad-bootstrap/browser');
 app.use(express.static(distFolder));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(distFolder, 'index.html'));
-});
-
-app.get('/favicon.ico', (req, res) => {
-  res.sendFile(path.join(distFolder, 'favicon.ico'));
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
