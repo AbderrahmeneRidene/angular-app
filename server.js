082d8e97d0342e0b739152f247c582ef7e929eb2
AbdRidene <abd.ridene@gmail.com>
2026-02-16 13:22:17 +0100

    uptate erve.js

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ const port = process.env.PORT || 8080;
 
 const distFolder = path.join(__dirname, 'dist/azure-ad-bootstrap/browser');
 
-// Servir Angular build
 app.use(express.static(distFolder));
 
-// Favicon fallback pour éviter les 503
-app.get('/favicon.ico', (req, res) => res.sendFile(path.join(distFolder, 'favicon.ico')));
-
-// Toutes les routes Angular redirigent vers index.html
 app.get('/*', (req, res) => {
   res.sendFile(path.join(distFolder, 'index.html'));
+});
+
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(distFolder, 'favicon.ico'));
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
