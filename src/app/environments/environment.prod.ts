ceb1d2302dffa67fcfea637eadfe4edb6721d8f4
AbdRidene <abd.ridene@gmail.com>
2026-02-17 16:40:20 +0100

    fisrt commit

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   azureAd: {
     clientId: '0bf05ae1-b1d8-4017-9959-1842d2f13150',
     tenantId: 'f75c19a9-c006-47d7-98cc-f1b5638e6af6',
-    redirectUri: 'https://angular-free-webapp-demo.azurewebsites.net/',
+    redirectUri: 'https://ridene-webapp-d3ffbxewfycddefu.eastus-01.azurewebsites.net/',
     authority: 'https://login.microsoftonline.com/f75c19a9-c006-47d7-98cc-f1b5638e6af6',
     scopes: ['user.read']
   }
